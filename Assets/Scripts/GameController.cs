cd3026124cb88cb2af9919b695b6568382e58779
Guilherme Caires <pessoalgcaires@gmail.com>
2021-08-12 07:09:41 -0300

    feat: add game over and restart game

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -2,11 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
     public int totalScore;
+
     public Text scoreText;
+    public GameObject gameOver;
     public static GameController instance;
 
     void Start()
@@ -17,5 +20,15 @@ public class GameController : MonoBehaviour
     public void UpdateScoreText()
     {
         scoreText.text = totalScore.ToString();
+    }
+
+    public void ShowGameOver()
+    {
+        gameOver.SetActive(true);
+    }
+
+    public void RestartGame(string levelName)
+    {
+        SceneManager.LoadScene(levelName);
     }
 }
