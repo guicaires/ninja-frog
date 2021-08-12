be5d5e665cd6a56580153aef9f2d758f7fc3015c
Guilherme Caires <pessoalgcaires@gmail.com>
2021-08-12 19:47:39 -0300

    feat: add next level object

---
+++ Assets/Scripts/NextLevel.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextLevel : MonoBehaviour
+{
+    public string levelName;
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene(levelName);
+        }
+    }
+}
